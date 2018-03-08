9e78f9abffca8b587581b7223f49f08137a5ea4d
James Marcus <james.marcus9@gmail.com>
2018-03-08 15:26:37 -0500

    working files for etch a sketch project

---
+++ etch-a-sketch.js
@@ -0,0 +1,68 @@
+// Etch-a-sketch js 
+
+
+container= document.querySelector('#container');
+
+const block1 = document.createElement('div');
+block1.classList.add('gridPiece');
+container.appendChild(block1);
+
+const block2 = document.createElement('div');
+block2.classList.add('gridPiece');
+container.appendChild(block2);
+
+const block3 = document.createElement('div');
+block3.classList.add('gridPiece');
+container.appendChild(block3);
+
+const block4 = document.createElement('div');
+block4.classList.add('gridPiece');
+container.appendChild(block4);
+
+const block5 = document.createElement('div');
+block5.classList.add('gridPiece');
+container.appendChild(block5);
+
+const block6 = document.createElement('div');
+block6.classList.add('gridPiece');
+container.appendChild(block6);
+
+const block7 = document.createElement('div');
+block7.classList.add('gridPiece');
+container.appendChild(block7);
+
+const block8 = document.createElement('div');
+block8.classList.add('gridPiece');
+container.appendChild(block8);
+
+const block9 = document.createElement('div');
+block9.classList.add('gridPiece');
+container.appendChild(block9);
+
+const block10 = document.createElement('div');
+block10.classList.add('gridPiece');
+container.appendChild(block10);
+
+const block11 = document.createElement('div');
+block11.classList.add('gridPiece');
+container.appendChild(block11);
+
+const block12 = document.createElement('div');
+block12.classList.add('gridPiece');
+container.appendChild(block12);
+
+const block13 = document.createElement('div');
+block13.classList.add('gridPiece');
+container.appendChild(block13);
+
+const block14 = document.createElement('div');
+block14.classList.add('gridPiece');
+container.appendChild(block14);
+
+const block15 = document.createElement('div');
+block15.classList.add('gridPiece');
+container.appendChild(block15);
+
+const block16 = document.createElement('div');
+block16.classList.add('gridPiece');
+container.appendChild(block16);
