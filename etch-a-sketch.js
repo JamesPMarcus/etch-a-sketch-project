7af90050bd4de69f00e982fe44b3476aa810093e
James Marcus <james.marcus9@gmail.com>
2018-03-10 20:39:08 -0500

    applied patch to js file

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,34 +1,45 @@
 // Etch-a-sketch js 
-
 //jquery is also used in this code
-
-
 //function for creating a grid, num is designate rows + columns
-
-
 function createGrid(num) {
-    for (var rows=0; rows < num; rows++) {
-        for (var columns=0; columns < num; columns++){
-            $('#container').append("<div class='grid'></div");
-             }        
+    var container = $("#container");
+    var overlay = $('.loading-overlay');
+    var containerWidth = container.width();
+    var containerHeight = container.height();
+
+    overlay.removeClass('hidden');
+
+    if (containerWidth > containerHeight) {
+        container.width(containerHeight);
+    } else {
+        container.height(containerWidth);
+    }
+
+    //defer the execution of the grid calculations to allow the DOM to refresh with loading
+    //Promises might be a better solution
+    setTimeout(function() {
+        for (var rows = 0; rows < num; rows++) {
+            var row = $("<div class='row'></div>")
+            for (var columns = 0; columns < num; columns++) {
+                row.append("<div class='grid'></div");
+            }
+            container.append(row);
         }
 
-$('.grid').width(960/num);
-$('.grid').height(960/num);
+        overlay.addClass('hidden');
+    }, 0);
+
 }
 
 //function to clear grid
-
 function gridClear() {
-    
-    $('.grid').remove();
+    $('#container').empty();
 }
-    
-    
-//function to refresh grid and ask for new grid dimension
 
+
+//function to refresh grid and ask for new grid dimension
 function refreshGrid() {
-    
+
     var dime = prompt("Choose your new dimensions");
     gridClear();
     createGrid(dime);
@@ -37,30 +48,20 @@ function refreshGrid() {
 //create a 16x16 grid when page loads
 //hover effect to change color of square
 //click of button to refrhesh Grid and ask for new dimensions
+$(document).ready(function() {
 
-
-$(document).ready(function () {
-    
     createGrid(16);
-    
+
     $('.grid').mouseover(function() {
-        $(this).css('background-color','black');
+        $(this).addClass('hovered');
+    });
+
+
+    $('.but').click(function() {
+        refreshGrid();
+
+        $('.grid').mouseover(function() {
+            $(this).css('background-color', 'black');
         });
-    
-        
-            $('.but').click (function () {
-                refreshGrid();
-               
-                 $('.grid').mouseover(function() {
-                $(this).css('background-color','black');
-                });
-    
-                
-            });
-        
- 
-    
-    
+    });
 });
-
-
